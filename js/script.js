fcc890726ad7810fcd2e63560ebf0a690c9f544a
Azaliya <azaliya-frbi@rambler.ru>
2022-01-10 22:39:52 +0300

    les28

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const box = document.getElementById("box");
+console.log(box);
+const btns = document.getElementsByTagName("button");
+console.log(btns);
+console.log(btns[2]);
+const btn2 = document.getElementsByTagName("button")[1];
+console.log(btn2);
+
+const circles = document.getElementsByClassName("circle");
+console.log(circles);
+console.log(circles[1]);
+
+const hearts = document.querySelectorAll(".heart");
+hearts.forEach((item) => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector(".heart");
+console.log(oneHeart);
